46a5fb329948e2ab9ed808e553c8d665860bb474
git config --global user.name Giovana <giovana.z.oliveira@aluno.senai.br>
2024-09-04 08:10:45 -0300

    oi

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicial():
+    return render_template("index.html")
+
+@app.route("/cardapio")
+def cardapio():
+    return render_template("cardapio.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
