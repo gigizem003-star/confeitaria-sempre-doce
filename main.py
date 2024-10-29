e76a851bf9f9a2f03fda356c18128df8168c0cc1
git config --global user.name Giovana <giovana.z.oliveira@aluno.senai.br>
2024-10-29 14:03:35 -0300

    oi

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from urllib import request
+
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = '/uploads'
 
 @app.route("/")
 def inicial():
@@ -18,6 +22,30 @@ def sobre():
 def contato():
     return render_template("contato.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+         return render_template("login.html")
+    elif request.method == "POST":
+        usuario = request.form.get("usuario")
+        senha = request.form.get("senha")
+
+        if usuario == "gigizem003@gmail.com":
+            if senha == "1234":
+                return redirect(url_for('upload'))
+
+    return render_template("login.html")
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+        if request.method == "GET":
+            return render_template("upload.html")
+        elif request.method == "POST":
+            file = request.files["file"]
+            caminho = app.config["UPLOAD_FOLDER"] + "/" + file.filename
+            file.save(caminho)
+
+            return render_template("cardapio.txt")
 
 if __name__ == '__main__':
     app.run(debug=True)
